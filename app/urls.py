fa904ee594f0994a4c701e053c65a8c6d6f5900c
mohammed <khuzaif254@gmail.com>
2024-11-24 20:00:24 +0530

    added app_urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
-
-from django.contrib import admin
 from django.urls import path
-
+from .views import ProductListCreateView,ProductRetrieveUpdateDestroyView,ProductByNameView,UserRegisterView
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products/',ProductListCreateView.as_view(),name='produc-list-create'),
+    path('products/<int:pk>/',ProductRetrieveUpdateDestroyView.as_view(),name='product-detail'),
+    path('products/name/<str:name>/',ProductByNameView.as_view(),name='product-by-name'),
+    path('register/',UserRegisterView.as_view(),name='register'),
+    
 ]
